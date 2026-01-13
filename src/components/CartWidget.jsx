cba8dbb1c444a04f44e903a37cb8f9a37da5d226
Gerard <ojedagerard5@gmail.com>
2026-01-13 13:50:16 -0300

    Primera entrega

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+function CartWidget() {
+  return (
+    <div className="relative cursor-pointer text-xl">
+      🛒 <span className="absolute -top-2 -right-3 bg-red-500 text-white text-xs rounded-full px-2">3</span>
+    </div>
+  );
+}
+
+export default CartWidget;
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+function ItemListContainer({ greeting }) {
+  return (
+    <div className="p-10 text-center">
+      <h3 className="text-2xl font-semibold text-gray-800">{greeting}</h3>
+    </div>
+  );
+}
+
+export default ItemListContainer;
